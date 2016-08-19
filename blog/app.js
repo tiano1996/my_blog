afbd111425dd577993516e902856ccb8e2f8376a
tiano1996 <circle4tiano@gmail.com>
2016-08-19 17:26:09 +0800

    准备搞flash

--- blog/app.js
+++ blog/app.js
@@ -4,15 +4,32 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var settings = require('./settings');
+var flash = require('connect-flash');
 
 var app = express();
+
+app.use(session({
+  resave: false, // fix the 'express-session' warring
+  saveUninitialized: true, // fix the 'express-session' warring
+  secret: settings.cookieSecret,
+  key: settings.db,
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},
+  store: new MongoStore({
+    url: 'mongodb://localhost:12345/blog'
+  })
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(flash());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +39,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+routes(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- blog/routes/index.js
+++ blog/routes/index.js
@@ -1,9 +1,52 @@
-var express = require('express');
-var router = express.Router();
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function(app) {
+  // 显示首页
+  app.get('/', function(req, res) {
+    res.render('index', {
+      blogTitle: 'My Blog',
+      title: '主页'
+    });
+  });
 
-module.exports = router;
+  // 显示注册页
+  app.get('/reg', function(req, res) {
+    res.render('reg', {
+      blogTitle: 'My Blog',
+      title: '注册新会员'
+    });
+  });
+
+  // 注册控制
+  app.get('/reg', function(req, res) {
+
+  });
+
+  // 显示登陆页
+  app.get('/login', function(req, res) {
+    res.render('login', {
+      blogTitle: 'My Blog',
+      title: '会员登陆'
+    });
+  });
+  // 登陆控制
+  app.get('/login', function(req, res) {
+
+  });
+
+  // 显示发布页
+  app.get('/post', function(req, res) {
+    res.render('post', {
+      blogTitle: 'My Blog',
+      title: '发布'
+  });
+  });
+  // 发布控制
+  app.get('/post', function(req, res) {
+
+  });
+
+  // 登出
+  app.get('/logout', function(req, res) {
+
+  });
+}
